a733439a61c15fb309a3f8e6526a5db8abd84df1
kobayashi-tbn <kobayashi.tbn@gmail.com>
2012-10-29 07:32:07 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoapp::Application.config.secret_token = '7206423f7490fbf555f97d359b6230ff7c49d471d388cf87b6a464d77ccb506f3ee170e5f5e265d1d15f97a9178c5f3a2028ffa373a6a153418cfa19cc81a737'
---
+++ db/migrate/20121028095627_create_todos.rb
@@ -0,0 +1,12 @@
+class CreateTodos < ActiveRecord::Migration
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.text :description
+      t.date :limit_on
+      t.boolean :done
+
+      t.timestamps
+    end
+  end
+end
