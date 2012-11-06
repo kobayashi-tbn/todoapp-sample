b616e57bc7c77e9fd6d3febefe882aaffce42606
kobayashi-tbn <kobayashi.tbn@gmail.com>
2012-11-06 23:15:59 +0900

    add validation to username field.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          :authentication_keys => [ :login ]
 
   validates_presence_of :username
-  
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login, :todos
   # attr_accessible :title, :body
