b0be050bcc7e6bfc9e50f802c8db6c24ae713766
kobayashi-tbn <kobayashi.tbn@gmail.com>
2012-12-01 14:57:02 +0900

    add scope for locale

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Todoapp::Application.routes.draw do
 
   devise_for :users
 
-  resources :todos
+  # resources :todos # move to scope declaration.
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -55,11 +55,18 @@ Todoapp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'home#index'
+  #root :to => 'home#index' #move to scope declaration.
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  # i18n
+  scope '(:locale)' do
+    #devise_for :users
+    resources :todos
+    root :to => 'home#index'
+  end
 end
