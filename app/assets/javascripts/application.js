9de2570abf9628876da81ec5c6df5fd143cf5969
kobayashi-tbn <kobayashi.tbn@gmail.com>
2012-12-01 14:35:33 +0900

    use jquery-ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,12 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
 //= require_tree .
+
+//$(".datepicker").datepicker();
+
+$(document).ready(function(){
+    $( ".datepicker" ).datepicker({dateFormat: 'yy-mm-dd'});
+});
