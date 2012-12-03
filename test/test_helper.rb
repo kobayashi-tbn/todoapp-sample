82572103a85c509961548286b573b8400bf2e864
kobayashi-tbn <kobayashi.tbn@gmail.com>
2012-12-04 00:20:00 +0900

    fix for travis ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,13 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+if ENV['DB'] == 'postgres'
+  configs = YAML.load_file('test/database.yml')
+  ActiveRecord::Base.configurations = configs
+  ActiveRecord::Base.establish_connection(configs['test'])
+  ActiveRecord::Base.default_timezone = :utc
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
