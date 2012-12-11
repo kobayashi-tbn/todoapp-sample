29d0618419386c54305d7d2a4a6e6fad2f93454f
kobayashi-tbn <kobayashi.tbn@gmail.com>
2012-12-11 23:49:29 +0900

    back to jquery-mobile from jqmobi

--- app/assets/javascripts/mobile/application.js
+++ app/assets/javascripts/mobile/application.js
@@ -10,57 +10,25 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-// require jquery
+//= require jquery
 // require ./custom
-// require ./jquery-mobile/jquery.mobile-1.2.0
-// require jquery_ujs
-//= require jq.mobi
-//= require jq.mobi_ujs
+//= require ./jquery-mobile/jquery.mobile-1.2.0
+//= require jquery_ujs
+// require jq.mobi
+// require jq.mobi_ujs
 // require jq.ui
-// require ./mobiscroll/mobiscroll-2.2.custom.min
+//= require ./mobiscroll/mobiscroll-2.2.custom.min
 // require_tree .
 
-//$(".datepicker").datepicker();
+//$(document).ready(function(){
+////    $( ".datepicker" ).datepicker({dateFormat: 'yy-mm-dd'});
+//
+//});
 
 $(document).ready(function(){
-//    $( ".datepicker" ).datepicker({dateFormat: 'yy-mm-dd'});
-
+    //create a datepicker with default settings
+    $("#scroller").scroller({ preset : 'date', dateFormat : 'yy-mm-dd',  dateOrder : 'yymmd' });
 });
 
-//$(document).ready(function(){
-//    //create a datepicker with default settings
-//    $("#scroller").scroller({ preset : 'date', dateFormat : 'yy-mm-dd',  dateOrder : 'yymmd' });
-//});
 
-//$(document).ready(function(){
-//    // create a datepicker with default settings
-////    jQuery.scroller.formatDate('yy:mm-dd',$(".scroller").val());
-////    alert($(".scroller").val());
-//    $(".date-selector").scroller({ dateFormat:'yy-mm-dd', dateOrder:'ymd' });
-//});
-
-//$(document).ready(function(){
-//
-//    var curr = new Date().getFullYear();
-//    var opt = {
-//
-//    }
-//
-//    opt.date = {preset : 'date'};
-//    opt.datetime = { preset : 'datetime', minDate: new Date(2012,3,10,9,22), maxDate: new Date(2014,7,30,15,44), stepMinute: 5  };
-//    opt.time = {preset : 'time'};
-//    <!--Script-->
-//
-//    $('select.changes').bind('change', function() {
-//        var demo = $('#demo').val();
-//        $(".demos").hide();
-//        if (!($("#demo_"+demo).length))
-//            demo = 'default';
-//
-//        $("#demo_" + demo).show();
-//        $('#scroller').val('').scroller('destroy').scroller($.extend(opt[$('#demo').val()], { theme: $('#theme').val(), mode: $('#mode').val(), display: $('#display').val(), lang: $('#language').val() }));
-//    });
-//
-//    $('#demo').trigger('change');
-//});
 
