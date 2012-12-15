932e8a467f04f096346c60d357be7215385ca19e
kobayashi-tbn <kobayashi.tbn@gmail.com>
2012-12-16 00:27:36 +0900

    add format :mobi for jqmobi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,16 +22,26 @@ class ApplicationController < ActionController::Base
 
   # For mobile site
   def mobile_device?
+    puts "user_agent #{request.user_agent}"
+    puts "session #{session}"
     if session[:mobile_param]
-      session[:mobile_param] == "1"
+      ["1", "2"].include? session[:mobile_param]
     else
-      request.user_agent =~ /android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/
+      request.user_agent =~ /android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|i(P|p)(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/
     end
   end
   helper_method :mobile_device?
 
   def prepare_for_mobile
+    #session[:mobile_param] = params[:mobile] if params[:mobile]
     session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
+    #request.format = :mobile if mobile_device?
+    if mobile_device?
+      if "2" == params[:mobile]
+        request.format = :mobile
+      else
+        request.format = :mobi
+      end
+    end
   end
 end
