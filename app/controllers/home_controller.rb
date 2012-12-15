9313ad610815a6d2228897ebdd97912dd5522e98
kobayashi-tbn <kobayashi.tbn@gmail.com>
2012-12-16 00:28:54 +0900

    add respond format

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   skip_before_filter :authenticate_user!
   def index
+    respond_to do |format|
+      format.html # index.html.erb
+      format.mobile # index.mobile.erb
+      format.mobi # index.mobi.erb
+      format.js   # for ajax
+      format.json { render json: @homes }
+    end
   end
 end
